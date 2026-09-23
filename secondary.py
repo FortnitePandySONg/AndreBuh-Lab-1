806e70faafcc930477569e92858215f66820ec04
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-23 17:43:59 +0000

    finished

--- secondary.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? ")
-print(name)
-
-if name == "Andre":
-    print("Hello Andre, you are better than Dominic at Tom Clancy Rainbow Six Siege")
-elif name == "Dominic":
-    input("Hello Dominic, you are very bad at siege")
-else:
-    print("Hello " + name + ", Andre is still better than you at Tom Clancy Rainbow Six Siege")

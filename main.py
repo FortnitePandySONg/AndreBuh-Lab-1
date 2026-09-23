f001a0a2077fd6b4d798728ba74c47964eaf4889
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-23 17:43:40 +0000

    finished

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(123+321)
+name = input("What is your name? ")
+print(name) 
+age = input("How old are you?")
+print(age)
+color = input("What is your favorite color?")
+print(color)
+input("Your name is " + name + ", you are " + age + " years old, and your favorite color is " + color)
+if age <= "16":
+    print("You are young and cool")
+else: 
+    print("Your are not old and wise")
